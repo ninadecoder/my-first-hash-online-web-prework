0792f2e02f7e5075e9ff85927540603be08a9346
Nina Kazzim <kazzim@icloud.com>
2019-06-05 20:23:09 +0000

    Automatically backed up by Learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -11,16 +11,16 @@ def my_hash
   
   end
 
-def retrieval
-  shipping_manifest = {
-                  "whale bone corsets" => 5, 
-                  "porcelain vases" => 2, 
-                  "oil paintings" => 3
-                 }
-
-  # Look up the value of the "oil paintings" key in the shipping_manifest hash below
-
-end
+ def retrieval
+    shipping_manifest = {
+                    "whale bone corsets" => 5, 
+                    "porcelain vases" => 2, 
+                    "oil paintings" => 3
+                   }
+  
+        shipping_manifest["oil paintings"]
+  
+  end
 
 def adding
   shipping_manifest = {
