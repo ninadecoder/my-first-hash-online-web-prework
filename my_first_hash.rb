845ced109dc059e6300ba06c3ca43fd9ad3e543e
Nina Kazzim <kazzim@icloud.com>
2019-06-05 19:43:07 +0000

    Automatically backed up by Learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -6,11 +6,7 @@ def my_hash
   
   
   def shipping_manifest
-    the_manifest = {
-                     "whale bone corsets" => 5,
-                    "porcelain vases" => 2,
-                    "oil Paintings" => 3 
-                    }
+    the_manifest = {"whale bone corsets" => 5, "porcelain vases" => 2,  "oil Paintings" => 3}
   
   
   end
