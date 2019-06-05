8517c0edec38ac4aaf4cfe994329b77a853b880c
Nina Kazzim <kazzim@icloud.com>
2019-06-05 20:32:48 +0000

    Automatically backed up by Learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -1,17 +1,19 @@
 def my_hash
    
-    your_hash = {"one" => 1, "name" => "Dami", "nickname" => "Nina"}
+    your_hash {"one" => 1, "name" => "Dami", "nickname" => "Nina"}
     
   end
   
   
   def shipping_manifest
-    the_manifest = {"whale bone corsets" => 5, "porcelain vases" => 2,  "oil paintings" => 3}
+    the_manifest = {
+                     "whale bone corsets" => 5, 2"porcelain vases" => 2,  "oil paintings" => 3 
+                    }
   
   
   end
-
- def retrieval
+  
+  def retrieval
     shipping_manifest = {
                     "whale bone corsets" => 5, 
                     "porcelain vases" => 2, 
@@ -21,8 +23,8 @@ def my_hash
         shipping_manifest["oil paintings"]
   
   end
-
-def adding
+  
+  def adding
     shipping_manifest = {
                     "whale bone corsets" => 5, 
                     "porcelain vases" => 2, 
