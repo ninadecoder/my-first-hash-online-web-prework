1cf57c6a106cac4451eb9039a253cbca79a08539
Nina Kazzim <kazzim@icloud.com>
2019-06-05 19:28:10 +0000

    Automatically backed up by Learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -1,17 +1,26 @@
 def my_hash
-  # use the literal constructor to set the variable, my_hash, equal to a hash with key/value pairs of your choice.
-
+   
+    your_hash {"one" => 1, "name" => "Dami", "nickname" => "Nina"}
+    
+  end
   
-end
-
-
-def shipping_manifest
-  # set a variable called `the_manifest`, equal to a hash
-  # fill that hash with key/value pairs that describe the following information: 
-  # We have 5 whale bone corsets, 2 porcelain vases and 3 oil paintings 
-
-
-end
+  
+  def shipping_manifest
+    the_manifest = {
+                     "Whale bone corsets" => 5,
+                    "Porcelain vases" => 2,
+                    "Oil Paintings" => 3 
+                    }
+  
+  
+  end
+  
+  def retrieval
+    shipping_manifest = {
+                    "whale bone corsets" => 5, 
+                    "porcelain vases" => 2, 
+                    "oil paintings" => 3
+                   }
 
 def retrieval
   shipping_manifest = {
